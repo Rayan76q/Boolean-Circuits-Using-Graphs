453c8ff60cfb6d0bfa086176f07349aca7470cb3
Rayan LALAOUI <rayan.lalaoui.768@gmail.com>
2024-02-02 10:17:42 +0100

    plus d'underscore

--- modules/open_digraph.py
+++ modules/open_digraph.py
@@ -11,10 +11,10 @@ class node:
         children: int->int dict; maps a child nodes id to its multiplicity 
         """
         
-        self._id = identity
-        self._label = label
-        self._parents = parents
-        self._children = children 
+        self.id = identity
+        self.label = label
+        self.parents = parents
+        self.children = children 
     
 
     def copy(self):
@@ -22,31 +22,31 @@ class node:
 
     
     def get_id(self):
-        return self._id
+        return self.id
     
     def get_label(self):
-        return self._label
+        return self.label
     
     def get_parents(self):
-        return self._parents
+        return self.parents
     
     def get_children(self):
-        return self._children   
+        return self.children   
 
     def set_id(self , id):
-        self._id = id
+        self.id = id
     
     def set_label(self, label):
-        self._label = label
+        self.label = label
     
     def set_children(self , children):
-        self._children = children
+        self.children = children
     
     def add_parent_id(self, par_id , multiplicity=1):
-        self._parents[par_id] = multiplicity
+        self.parents[par_id] = multiplicity
     
     def add_child_id(self, child_id , multiplicity=1):
-        self._children[child_id] = multiplicity
+        self.children[child_id] = multiplicity
 
     def __str__ (self):
         return f"Node {self.id}: \nLabel : {self.label}\nParents : {self.children}\nChildre : {self.children}"
@@ -65,9 +65,9 @@ class open_digraph: # for open directed graph
         outputs: int list; the ids of the output nodes
         nodes: node iter;
         """
-        self._inputs = inputs
-        self._outputs = outputs
-        self._nodes = {node.id:node for node in nodes} 
+        self.inputs = inputs
+        self.outputs = outputs
+        self.nodes = {node.id:node for node in nodes} 
 
 
     @classmethod
@@ -75,37 +75,37 @@ class open_digraph: # for open directed graph
         return open_digraph([],[],{})
 
     def get_inputs_ids(self):
-        return self._inputs
+        return self.inputs
 
     def get_outputs_ids(self):
-        return self._outputs
+        return self.outputs
 
     def get_id_node_map(self):
-        return self._nodes
+        return self.nodes
 
     def get_nodes(self):
-        return self._nodes.values()
+        return self.nodes.values()
 
     def get_node_ids(self):
         return self._nodes.keys()
 
     def get_node_by_id(self,id):
-        return self._nodes[id]
+        return self.nodes[id]
     
     def get_nodes_by_ids(self , ids):
-        return [self._nodes[id] for id in ids]
+        return [self.nodes[id] for id in ids]
     
     def set_inputs(self , inputs):
-        self._inputs = inputs
+        self.inputs = inputs
     
     def set_outputs(self , outputs):
-        self._outputs = outputs
+        self.outputs = outputs
 
     def add_input_id(self , id):
-        self._inputs.append(id)
+        self.inputs.append(id)
 
     def add_output_id(self , id):
-        self._outputs.append(id)
+        self.outputs.append(id)
     
 
     
@@ -158,9 +158,8 @@ inp= [1,2,3]
 outputs = [5]
 g = open_digraph(inp , outputs , n0)
 n = node(0, 'i', {}, {1:1})
-print(n)
-n.id = 1
-print(n)
+print(g)
+
             
 """if __name__ == '__main__': # the following code is called only when
     unittest.main() """
